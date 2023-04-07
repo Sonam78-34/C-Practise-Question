c3a537dd7a56f4bf2339701addfc5c4a2e796258
Sonam Kumari <126173613+Sonam78-34@users.noreply.github.com>
2023-04-07 18:32:35 +0530

    Add files via upload

---
+++ mixed Array of number and string/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+
+namespace mixed_Array_of_number_and_string
+{
+    public class Program 
+    { 
+        static void Main(string[] args) 
+        { ArrayList arrlist = new ArrayList() { 1, "2", 3, '4' };
+            int sum = 0; 
+            int sum2 = 0;
+            int num; 
+            foreach (var value in arrlist)
+            { 
+                Type tp = value.GetType();
+                if (tp.Equals(typeof(int))) 
+                { int num1 = Convert.ToInt32(value);
+                    sum = sum + num1; 
+                } 
+                else 
+                { 
+                    string string2 = ""; 
+                    string2 = value.ToString();
+                    num = Int16.Parse(string2);
+                    sum2 = sum2 + num; 
+                } 
+            } 
+            Console.WriteLine("Integer element total is {0} and String element total is {1} Then difference is {2}", sum, sum2, Math.Abs(sum - sum2)); 
+        }
+    } 
+}
