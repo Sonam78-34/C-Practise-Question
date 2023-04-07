788a16cd399ee428bd234d03778f4a6126e9c2bf
Sonam Kumari <126173613+Sonam78-34@users.noreply.github.com>
2023-04-07 18:43:49 +0530

    Add files via upload

---
+++ multilevel inheritance/Multinheritance.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+namespace multilevel_inheritance
+{
+    public class Animal
+    {
+        public String typeOfAnimal;
+        public String color;
+        public int age;
+
+    }
+    public class Dog : Animal
+    {
+         public int speed;
+        public void  wark()
+        {
+            Console.WriteLine("The dog is warking");
+        }
+    }
+    public class puppy : Dog
+    {
+        public void eating()
+        {
+            Console.WriteLine("The puppy is eating");
+        }
+    }
+
+} 
---
+++ multilevel inheritance/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace multilevel_inheritance
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            puppy smallDog= new puppy();
+            smallDog.typeOfAnimal= "razo";
+            smallDog.color = "brown";
+            smallDog.speed = 123;
+            smallDog.age = 2;
+            smallDog.eating();
+            smallDog.wark();
+            Console.WriteLine(smallDog.speed);
+        }
+    }
+}
