7ff9330005a2786e044e81e2600e29e22d57c804
Sonam Kumari <126173613+Sonam78-34@users.noreply.github.com>
2023-04-07 18:35:29 +0530

    Add files via upload

---
+++ String searching/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace String_searching
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("length");
+            int len= int.Parse(Console.ReadLine());
+            Console.WriteLine("enter the string array");
+            
+            string[] str=new string[len];
+            for(int i=0;i<len; i++)
+            {
+                str[i] = Console.ReadLine();
+            }
+            
+            int flag = 0;
+            for (int i = 0; i < (str.Length) - 1; i++)
+            {
+                string string2 = "";
+                string string3 = "";
+                char last = '\0';
+                char first = '\0';
+                string2 = str[i];
+                string3 = str[i + 1];
+                last = string2[(string2.Length) - 1];
+                first = string3[0];
+                Console.WriteLine(first);
+                Console.WriteLine(last);
+                if (last.Equals(first))
+                {
+                    flag = 0;
+                }
+                else
+                {
+                    flag = 1;
+                }
+            }
+            if (flag == 0)
+            {
+                Console.WriteLine("YES");
+            }
+            else
+            {
+                Console.WriteLine("NO");
+            }
+        }
+    }
+}
