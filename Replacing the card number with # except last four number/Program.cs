3a09818706e7bd4cfa2675fd891449cf387e9243
Sonam Kumari <126173613+Sonam78-34@users.noreply.github.com>
2023-04-07 17:48:38 +0530

    Add files via upload

---
+++ Replacing the card number with # except last four number/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Replacing_the_card_number_with___except_last_four_number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the card number");
+            String cardNumber= Console.ReadLine();
+            String maskNumber = " ";
+            if(cardNumber.Length<=4) 
+            {
+                Console.WriteLine(cardNumber);
+            }
+            else
+            {
+                for(int cardDigit= 0;cardDigit<cardNumber.Length-4;cardDigit++)
+                {
+                    maskNumber+="#";
+                }
+                maskNumber += cardNumber.Substring(cardNumber.Length - 4);
+                Console.WriteLine(maskNumber);
+
+            }
+           
+        }
+    }
+}
