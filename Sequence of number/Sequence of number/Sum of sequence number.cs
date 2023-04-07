242dfe2915ab2c2b6594a2ff54364f00d8fa2896
Sonam Kumari <126173613+Sonam78-34@users.noreply.github.com>
2023-04-07 17:50:06 +0530

    Add files via upload

---
+++ Sequence of number/Sequence of number/Sum of sequence number.cs
@@ -0,0 +1,29 @@
+﻿namespace Sequence_of_number
+{
+    public class Block
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the start number");
+            int startNumber= int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter the end number");
+            int endNumber= int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter the steps");
+            int steps= int.Parse(Console.ReadLine());
+
+            int sum=0;
+            if(startNumber==endNumber)
+            {
+                Console.WriteLine("0");
+            }
+            for(int number=startNumber; number<=endNumber; number+=steps)
+            {
+                sum += number;
+            }
+            Console.WriteLine(sum);
+
+        }
+    }
+}
