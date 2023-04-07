68fc16173834a605a31702d8bd7e2feec627af84
Sonam Kumari <126173613+Sonam78-34@users.noreply.github.com>
2023-04-07 17:46:49 +0530

    Add files via upload

---
+++ Remove vowels/Remove vowels/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Remove_vowels
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter the String");
+            string previousString = Console.ReadLine();
+            string modifiedString = "";
+
+            for (int i = 0; i < previousString.Length; i++)
+            {
+                char c= previousString[i];
+                if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' ||
+                    c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U')
+                {
+                    continue; // skip vowels
+                }
+                modifiedString += c;
+
+            }
+
+            Console.WriteLine("Original string: " + prevString);
+            Console.WriteLine("Updated string: " + modifiedString);
+
+
+
+        }
+    }
+}
