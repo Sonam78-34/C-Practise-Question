c1ef352ed019a5d9b5c3ebe58be73744710bcede
Sonam Kumari <126173613+Sonam78-34@users.noreply.github.com>
2023-04-07 17:41:20 +0530

    Add files via upload

---
+++ Remove paranthesis/Remove paranthesis/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Remove_paranthesis
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the string");
+            String originalString = Console.ReadLine();
+            String updatedstring = "";
+            for (int i = 0; i < OriginalString.Length; i++)
+            {
+                if (originalStringi] == '(')
+                {
+
+                    for (char c = originalString[i]; originalString[i] != ')'; i++)
+                    {
+                        continue;
+                    }
+
+                }
+
+                else
+                {
+                    updatedstr += originalString[i];
+                }
+
+            }
+            Console.WriteLine(updatedstr);
+
+        }
+    }
+}
