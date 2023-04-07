16ab2a90a998075b6f6b5ab8605c6f6b54c6c72b
Sonam Kumari <126173613+Sonam78-34@users.noreply.github.com>
2023-04-07 17:37:42 +0530

    Add files via upload

---
+++ hierarchial inheritance/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace hierarchial_inheritance
+{
+    
+    public class Father
+    {
+        public string FatherName()
+        {
+            return "Ravi";
+        }
+    }
+
+    
+    public class ChildFirst : Father
+    {
+        public string ChildDName()
+        {
+            return "Rohan";
+        }
+    }
+
+    
+    public class ChildSecond : Father
+    {
+        public string ChildDName()
+        {
+            return "Nikhil";
+        }
+    }
+    public class Program
+    {
+        public static  void Main()
+        {
+            ChildFirst first = new ChildFirst();
+
+            Console.WriteLine("My name is " + first.ChildDName() +
+                              ". My father name is " +
+                              first.FatherName() + ".");
+            ChildSecond second = new ChildSecond();
+
+           
+            Console.WriteLine("My name is " + second.ChildDName() +
+                              ". My father name is " +
+                              second.FatherName() + ".");
+        }
+    }
+}
