6cddc49f99d95d63cf9fe9703f64e45b66100100
Sonam Kumari <126173613+Sonam78-34@users.noreply.github.com>
2023-04-07 18:24:40 +0530

    Add files via upload

---
+++ Occurances of character/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Occurances_of_character
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the string : ");
+            string stringValue = Console.ReadLine();
+
+            stringValue = stringValue.Replace(" ", string.Empty);
+            Console.WriteLine(stringValue);
+
+            while (stringValue.Length > 0)
+            {
+
+                int count = 0;
+                for (int j = 0; j < stringValue.Length; j++)
+                {
+                    if (stringValue[0] == stringValue[j])
+                    {
+                        count++;
+                    }
+                }
+                Console.Write(stringValue[0] + " : " + count + ",");
+                stringValue = stringValue.Replace(stringValue[0].ToString(), string.Empty);
+
+            }
+        }
+    }
+}
