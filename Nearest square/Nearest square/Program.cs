1caa85011663ac70617aeec737367374f9eeeaac
Sonam Kumari <126173613+Sonam78-34@users.noreply.github.com>
2023-04-07 17:31:22 +0530

    Add files via upload

---
+++ Nearest square/Nearest square/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Nearest_square
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter The Number");
+            int number = int.Parse(Console.ReadLine());
+            int i = 1;
+            int lowerSqaure = 1, upperSquare = 1;
+            while (number > i * i)
+            {
+                if (i * i != number)
+                {
+
+                    lowerSqaure = i;
+                    upperSquare = i+1;
+                }
+                i++;
+            }
+            if (i * i == num)
+            {
+                Console.WriteLine(i);
+            }
+            else
+            {
+                Console.WriteLine( lowerSqaure);
+                Console.WriteLine(upperSquare);
+            }
+
+        }
+    }
+}
