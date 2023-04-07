d5c427af0db0c9f5dd697b6e4230e3a26f4618b4
Sonam Kumari <126173613+Sonam78-34@users.noreply.github.com>
2023-04-07 17:23:53 +0530

    Add files via upload

---
+++ Bandname/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Bandname
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the noun");
+            String noun = Console.ReadLine();
+            String bandName = " ";
+
+
+            if (noun[0] == noun[noun.Length - 1])
+            {
+                char firstcharacter = char.ToUpper(noun[0]);
+                bandName = firstcharacter + noun.Substring(1) + noun;
+
+            }
+            else
+            {
+                bandName = "The " + noun;
+
+            }
+            Console.WriteLine(bandName);
+
+
+        }
+    }
+}
