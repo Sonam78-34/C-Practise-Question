d1acf7265f9ef65fdcfca0b6de5d648711975289
Sonam Kumari <126173613+Sonam78-34@users.noreply.github.com>
2023-04-10 10:34:37 +0530

    Add files via upload

---
+++ Find maximum  occurance of charcter/findevenandoddstring/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace findevenandoddstring
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the string");
+            String str= Console.ReadLine();
+            /*
+            String evenstr = "";
+            String oddstr = " ";
+            for(int i=0;i<str.Length;i++)
+            {
+                if (i%2==0)
+                {
+                    evenstr =evenstr+ str[i];
+                    
+                }
+                else
+                {
+                    oddstr += str[i];   
+                }
+            }
+           Console.WriteLine(evenstr+" "+oddstr);
+            ===============================*/
+            String updatedstr = "";
+            for(int i=0;i<str.Length;i++)
+            {
+                if (str[i]=='(')
+                {
+                    
+                    for(char c = str[i];str[i]!=')';i++)
+                    {
+                        continue;
+                    }
+
+                }
+               
+                else
+                {
+                    updatedstr += str[i];
+                }
+                
+            }
+            Console.WriteLine(updatedstr);
+            
+        }
+    }
+}
