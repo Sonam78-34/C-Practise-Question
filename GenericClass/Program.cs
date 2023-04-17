f9ba5992f1988dd1e67a8ab2299c4ab73c1ebb35
Sonam Kumari <126173613+Sonam78-34@users.noreply.github.com>
2023-04-17 17:45:46 +0530

    Add files via upload

---
+++ GenericClass/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace GenericClass
+{
+    using System;
+
+    public class MyGenericClass<T>
+    {
+        private T myVariable;
+
+        public MyGenericClass(T variable)
+        {
+            myVariable = variable;
+        }
+
+        public T GetVariable()
+        {
+            return myVariable;
+        }
+
+        public void SetVariable(T variable)
+        {
+            myVariable = variable;
+        }
+    }
+
+    public class Program
+    {
+        public static void Main()
+        {
+            MyGenericClass<int> myIntClass = new MyGenericClass<int>(42);
+            Console.WriteLine("The IntClass variable value is " + myIntClass.GetVariable());
+
+            MyGenericClass<string> myStringClass = new MyGenericClass<string>("Sia");
+            Console.WriteLine("The string class variable value: " + myStringClass.GetVariable());
+
+            
+            myStringClass.SetVariable("Sonam ");
+            Console.WriteLine("The string class  variable value: " + myStringClass.GetVariable());
+        }
+    }
+
+}
