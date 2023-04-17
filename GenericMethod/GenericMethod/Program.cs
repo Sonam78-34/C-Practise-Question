6c0aba350cf62b6e9ce3f113e30da684da699506
Sonam Kumari <126173613+Sonam78-34@users.noreply.github.com>
2023-04-17 17:53:07 +0530

    Add files via upload
    Add the file

---
+++ GenericMethod/GenericMethod/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace GenericMethod
+{
+    using System;
+
+    class Program
+    {
+         public static void Main(string[] args)
+        {
+            int[] numbers = { 1, 2, 3, 4, 5 };
+            string[] words = { "apple", "banana", "cherry" };
+
+            Console.WriteLine("First element of numbers: " + First(numbers));
+            Console.WriteLine("First element of words: " + First(words));
+        }
+
+         public static T First<T>(T[] numberArray)
+        {
+            if (numberArray.Length == 0)
+            {
+                throw new InvalidOperationException("Array is empty");
+            }
+
+            return numberArray[0];
+        }
+    }
+
+}
