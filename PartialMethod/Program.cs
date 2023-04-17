a4a44b6f1b23364c86434fc8b3bd28202ed4a0f8
Sonam Kumari <126173613+Sonam78-34@users.noreply.github.com>
2023-04-17 17:31:52 +0530

    Add files via upload

---
+++ PartialMethod/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace PartialMethod
+{
+    public partial class AddNumber
+    {
+        public partial void AddTwoNumber(int number1, int number2);
+       
+    }
+    public partial class AddNumber
+
+    {
+        public partial void AddTwoNumber(int number1,int number2)
+        {
+            Console.WriteLine(number1+number2);
+
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            AddNumber add= new AddNumber();
+            int number1 = 10;
+            int number2 = 20;
+            add.AddTwoNumber(10,20);
+
+        }
+    }
+}
