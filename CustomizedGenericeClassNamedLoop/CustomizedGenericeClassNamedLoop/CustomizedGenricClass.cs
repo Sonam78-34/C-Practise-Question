65b548c69c7bed976e84f9ef840027b81c3e346a
Sonam Kumari <126173613+Sonam78-34@users.noreply.github.com>
2023-04-17 18:03:48 +0530

    Add files via upload
    add the folder

---
+++ CustomizedGenericeClassNamedLoop/CustomizedGenericeClassNamedLoop/CustomizedGenricClass.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomizedGenericeClassNamedLoop
+{
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+
+    class Loop<T> : IEnumerable<T>
+    {
+        private  List<T> items = new List<T>();
+        private int index = 0;
+
+        public Loop()
+        { 
+        }
+
+        public Loop(IEnumerable<T> collection)
+        {
+            foreach (T item in collection)
+            {
+                items.Add(item);
+            }
+        }
+
+        public void Add(T item)
+        {
+            items.Add(item);
+        }
+
+        public void RotateRight()
+        {
+            index--;
+            if (index < 0)
+            {
+                index = items.Count - 1;
+            }
+        }
+
+        public void RotateLeft()
+        {
+            index++;
+            if (index >= items.Count)
+            {
+                index = 0;
+            }
+        }
+
+        public T ShowFirst()
+        {
+            return items[index];
+        }
+
+        public T PopOut()
+        {
+            T item = items[index];
+            items.RemoveAt(index);
+            if (index >= items.Count)
+            {
+                index = 0;
+            }
+            return item;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return new LoopEnumerator(this);
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        private class LoopEnumerator : IEnumerator<T>
+        {
+            private readonly Loop<T> loop;
+            private int index;
+
+            public LoopEnumerator(Loop<T> loop)
+            {
+                this.loop = loop;
+                index = loop.index;
+            }
+
+            public T Current
+            {
+                get
+                {
+                    try
+                    {
+                        return loop.items[index];
+                    }
+                    catch (IndexOutOfRangeException)
+                    {
+                        throw new InvalidOperationException();
+                    }
+                }
+            }
+
+            object IEnumerator.Current => Current;
+
+            public void Dispose() { }
+
+            public bool MoveNext()
+            {
+                index++;
+                if (index >= loop.items.Count)
+                {
+                    index = 0;
+                }
+                return index != loop.index;
+            }
+
+            public void Reset()
+            {
+                index = loop.index;
+            }
+        }
+    }
+
+   
+    
+
+}
---
+++ CustomizedGenericeClassNamedLoop/CustomizedGenericeClassNamedLoop/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace CustomizedGenericeClassNamedLoop
+{
+    public class Program
+    {
+         public static void Main(string[] args)
+        {
+            Loop<int> loop = new Loop<int> { 1, 2, 3, 4, 5 };
+
+            Console.WriteLine("ShowFirst: " + loop.ShowFirst());
+
+            Console.WriteLine("PopOut: " + loop.PopOut());
+
+            loop.Add(6);
+
+            Console.WriteLine("RotateRight:");
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine(loop.ShowFirst());
+                loop.RotateRight();
+            }
+
+            Console.WriteLine("RotateLeft:");
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine(loop.ShowFirst());
+                loop.RotateLeft();
+            }
+
+            Console.WriteLine("Loop with foreach:");
+            foreach (int i in loop)
+            {
+                Console.WriteLine(i);
+                if (i == 4)
+                {
+                    loop.PopOut();
+                    loop.Add(7);
+                }
+            }
+        }
+    }
+}
