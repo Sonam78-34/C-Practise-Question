a856b54af6536f2e5b009957aa1cefb28a5bbe1e
Sonam Kumari <126173613+Sonam78-34@users.noreply.github.com>
2023-04-17 18:11:24 +0530

    Add files via upload
    add the  folder

---
+++ fooClass/Program.cs
@@ -0,0 +1,51 @@
+﻿namespace fooClass
+{
+    using System;
+    using System.Threading;
+
+    public class Foo
+    {
+        private ManualResetEvent firstEvent = new ManualResetEvent(false);
+        private ManualResetEvent secondEvent = new ManualResetEvent(false);
+
+        public void first()
+        {
+            Console.WriteLine("first");
+            firstEvent.Set(); 
+        }
+
+        public void second()
+        {
+            firstEvent.WaitOne(); 
+            Console.WriteLine("second");
+            secondEvent.Set(); 
+        }
+
+        public void third()
+        {
+            secondEvent.WaitOne(); 
+            Console.WriteLine("third");
+        }
+    }
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Foo foo = new Foo();
+
+            Thread threadA = new Thread(new ThreadStart(foo.first));
+            Thread threadB = new Thread(new ThreadStart(foo.second));
+            Thread threadC = new Thread(new ThreadStart(foo.third));
+
+            threadA.Start();
+            threadB.Start();
+            threadC.Start();
+
+            threadA.Join();
+            threadB.Join();
+            threadC.Join();
+        }
+    }
+
+}
